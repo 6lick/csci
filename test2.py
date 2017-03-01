9515e43c5cffb8dd4de44e67dfaad9a0826d4f2f
Decker-Russell <rude5139@colorado.edu>
2017-03-01 13:27:50 -0700

    attempting huffman tree

--- test2.py
+++ test2.py
@@ -6,27 +6,17 @@ class minheap(object):
 	def insert(self,value, f):
 		self.heap.append(f)
 		self.org()
-		key.append(value)
-		key[value]=value
-		codebook=dict(zip(key,self.heap))
 		
-		#print(codebook)
 	
 		
 				
 	def deletemin(self):
 		a = self.heap[0]
 		del self.heap[0]
-		codebook.pop
 		self.org()
 		return a
 		
 	def org(self):
-		#for i in range(0,len(self.heap)):
-			#key.append(i)
-			#key[i]=i
-			#codebook=dict(zip(key,self.heap))
-			#print(codebook)
 		for i in range(len(self.heap)):
 			if (2*i)+1 >= len(self.heap):
 				return
@@ -73,24 +63,40 @@ def string2freq(x):
                     
         return S,f
 
-
+class tree(minheap):
+        def __init__(self):
+                self.left=None
+                self.right=None
+                self.data=None
+	def printTree(self):
+		if root.left.data
+			printTree
 
 def huffmanEncode(S,f):
 	H = minheap()
+	hTree=tree()
 	for i in range(len(S)):
 			H.insert(i, f[i])
 	for k in range(len(S),2*len(S)-2):
 			i = H.deletemin()
 			j = H.deletemin()
+
+			hTree.left=tree()
+			hTree.left.data=i
+
+			hTree.right=tree()
+			hTree.right.data=j
+			
+			hTree.data=hTree.left.data + hTree.right.data
 			f.append(f[i] + f[j])
-			H.insert(k, f[k])
+                        H.insert(k, f[k])
+
 	return codebook
+	
 	
 def main():
 		x = string2freq("Hello World")
 		
 		print(huffmanEncode(x[0],x[1]))
-
 if __name__ == "__main__":main()			
 				 
-	
